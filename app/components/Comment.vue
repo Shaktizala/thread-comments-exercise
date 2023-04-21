9dcef94ce8d0482e5f3657b4e5746ae1a8132290
Shaktiraj <shaktirajsinh.zala@improwised.com>
2023-04-21 19:41:15 +0530

    starting

---
+++ app/components/Comment.vue
@@ -0,0 +1,31 @@
+<template>
+  <span>{{ comment }}</span>
+
+  <ul v-if="replies.length">
+    <li>
+      <div v-for="(item, index) in replies" :key="index">
+        <Comment
+          v-bind="{
+            comment: item.comment,
+            replies: item.comments,
+          }"
+        />
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  comment: {
+    type: String,
+    required: true,
+  },
+  replies: {
+    type: Array,
+    default: () => [],
+  },
+})
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,29 @@
 <script setup>
-/*Define Pinia store*/
-/*import { useUsersStore } from '~/store/users'
-const UsersStore = useUsersStore()
-console.log(UsersStore);*/
+let message = ref('')
+let isReply = ref(true)
+let arraycomment = ref()
+
+let { data: comments, pending } = useLazyAsyncData('place', () =>
+  $fetch('http://localhost:5000/comments')
+)
+arraycomment.value = comments.value.comment
+function addToArray() {
+  arraycomment.value.push(message.value)
+  message.value = ''
+}
 </script>
 <template>
-  <div class="text-center homepage">Welcome to nuxt3</div>
+  <div class="d-block">
+    <input v-model="message" type="text" name="comment" class="m-3" />
+    <button class="btn btn-primary" @click="addToArray()">Add</button>
+  </div>
+  <ul v-for="(item, index) in comments" :key="index">
+    <Comment
+      v-bind="{
+        comment: item.comment,
+        replies: item.comments,
+      }"
+    />
+  </ul>
 </template>
-<style scoped>
-.homepage {
-  margin-top: 400px;
-  font-size: 30px;
-}
-</style>
+<style scoped></style>
