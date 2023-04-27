d0285b2c1dd1f3187e627fe3ea8cb4499de0e184
Shaktiraj <shaktirajsinh.zala@improwised.com>
2023-04-27 18:43:54 +0530

    thread comment without prop mutation error

--- app/components/Comment.vue
+++ app/components/Comment.vue
@@ -1,23 +1,30 @@
 <template>
-  <span>{{ comment }}</span>
-
-  <ul v-if="replies.length">
-    <li>
-      <div v-for="(item, index) in replies" :key="index">
-        <Comment
-          v-bind="{
-            comment: item.comment,
-            replies: item.comments,
-          }"
-        />
+  <div class="m-3">
+    <p>{{ comment }}</p>
+    <button @click="toggleInput()">reply</button>
+    <div v-if="replies.length">
+      <div class="ms-5">
+        <div class="d-block" :class="!isReply ? 'd-none' : ''">
+          <input v-model="message" type="text" name="comment" class="m-3" />
+          <button class="btn btn-primary" @click="addToArray()">Add</button>
+        </div>
+        <div v-for="(item, index) in replies" :key="index">
+          <Comment
+            v-bind="{
+              comment: item.comment,
+              replies: item.comments,
+            }"
+          />
+        </div>
       </div>
-    </li>
-  </ul>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { defineProps } from 'vue'
-
+let message = ref('')
+let isReply = ref(false)
 const props = defineProps({
   comment: {
     type: String,
@@ -28,4 +35,23 @@ const props = defineProps({
     default: () => [],
   },
 })
+
+function addToArray() {
+  const arrObj = {
+    _id: '',
+    picture: '',
+    auther: '',
+    gender: '',
+    comment: message.value,
+    liked: true,
+    isActive: true,
+  }
+  console.log(props.replies)
+  this.props.replies.push(arrObj)
+  message.value = ''
+}
+
+function toggleInput() {
+  isReply.value = !isReply.value
+}
 </script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,29 +1,39 @@
 <script setup>
 let message = ref('')
-let isReply = ref(true)
 let arraycomment = ref()
 
 let { data: comments, pending } = useLazyAsyncData('place', () =>
   $fetch('http://localhost:5000/comments')
 )
-arraycomment.value = comments.value.comment
+arraycomment.value = comments.value
 function addToArray() {
-  arraycomment.value.push(message.value)
+  const arrObj = {
+    _id: '',
+    picture: '',
+    auther: '',
+    gender: '',
+    comment: message.value,
+    liked: true,
+    isActive: true,
+  }
+  arraycomment.value.push(arrObj)
   message.value = ''
 }
 </script>
 <template>
-  <div class="d-block">
-    <input v-model="message" type="text" name="comment" class="m-3" />
-    <button class="btn btn-primary" @click="addToArray()">Add</button>
+  <div class="ms-5">
+    <div class="d-block">
+      <input v-model="message" type="text" name="comment" class="m-3" />
+      <button class="btn btn-primary" @click="addToArray()">Add</button>
+    </div>
+    <div v-for="(item, index) in comments" :key="index">
+      <Comment
+        v-bind="{
+          comment: item.comment,
+          replies: item.comments,
+        }"
+      />
+    </div>
   </div>
-  <ul v-for="(item, index) in comments" :key="index">
-    <Comment
-      v-bind="{
-        comment: item.comment,
-        replies: item.comments,
-      }"
-    />
-  </ul>
 </template>
 <style scoped></style>
